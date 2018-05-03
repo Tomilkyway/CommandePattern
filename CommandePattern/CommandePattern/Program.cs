7edda6b2f62b1bd777b58073adb58a57ee0a0c7a
Thomas Pain-Surget <thomas.painsurget@gmail.com>
2018-05-03 16:46:56 +0200

    test first commit

---
+++ CommandePattern/CommandePattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
