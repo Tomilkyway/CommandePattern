f527b1ba9cc136389b21a14f5350e1aead0b418c
Thomas Pain-Surget <thomas.painsurget@gmail.com>
2018-05-05 23:13:20 +0200

    Construction of Article DataBase Object

--- CommandePattern/CommandePattern/Article.cs
+++ CommandePattern/CommandePattern/Article.cs
@@ -7,5 +7,24 @@ namespace CommandePattern
         public string title { get; set; }
         public string content { get; set; }
 
-    }
+        public Article(){
+            
+        }
+
+        public Article(Article article) {
+            this.id = article.id;
+            this.title = article.title;
+            this.content = article.content;
+        }
+
+        public Article(String title, String content) {
+            this.title = title;
+            this.content = content;
+        }
+
+		public override string ToString() {
+            return "[Article] {id : " + id + "} \n {title : " + title + "} \n {content: " + content +"} \n\n";
+		}
+
+	}
 }
